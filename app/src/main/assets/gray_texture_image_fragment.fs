e0be8dd286163ec87e9560bc1b6f2b6099d7d34b
renzhenming <1565766177@qq.com>
2024-08-23 07:40:03 +0800

    feat：纹理贴图(黑白滤镜)

--- app/src/main/assets/gray_texture_image_fragment.fs
+++ app/src/main/assets/gray_texture_image_fragment.fs
@@ -8,6 +8,9 @@ varying vec4 v_Position;
 void main(){
     vec4 originColor = texture2D(vTexture,aCoordinate);
     if(v_Position.x > 0.0){
+        //黑白图片上，每个像素点的RGB三个通道值应该是相等的。知道了这个，将彩色图片处理成黑白图片就非常简单了。
+        //我们直接出处像素点的RGB三个通道，相加然后除以3作为处理后每个通道的值就可以得到一个黑白图片了。
+        //这是均值的方式是常见黑白图片处理的一种方法。类似的还有权值方法（给予RGB三个通道不同的比例）、只取绿色通道等方式。
         float v = originColor.r*0.3 + originColor.g * 0.59+originColor.b*0.11;//Gray = R * 0.3 + G * 0.59 + B * 0.11
         //float v = originColor.g;//仅取绿色： Gray = G
         gl_FragColor = vec4(v,v,v,originColor.a);
