dbadb30ba423dd4b36d64eb7910ef079efd69143
renzhenming <1565766177@qq.com>
2024-09-06 08:11:10 +0800

    feat：优化正方形代码

--- app/src/main/cpp/SquareRender.cpp
+++ app/src/main/cpp/SquareRender.cpp
@@ -17,6 +17,11 @@ void SquareRender::CreateSurface() {
     unsigned char *fragment_shader_code = loader->LoadFile("triangle_fragment_shader.fs",
                                                            file_size);
     program = CreateProgram(vertex_shader_code, fragment_shader_code);
+    positionLocation = glGetAttribLocation(program, "vPosition");
+    modelMatrixLocation = glGetUniformLocation(program, "U_ModelMatrix");
+    viewMatrixLocation = glGetUniformLocation(program, "U_ViewMatrix");
+    projectionMatrixLocation = glGetUniformLocation(program, "U_ProjectionMatrix");
+    colorLocation = glGetUniformLocation(program, "vColor");
     modelMatrix = glm::translate(0.0f, 0.0f, 0.0f);
     viewMatrix = glm::lookAt(glm::vec3(0.0f, 0.0f, 7.0f), glm::vec3(0.0, 0.0, 0.0f),
                              glm::vec3(0.0, 1.0f, 0.0f));
@@ -24,28 +29,28 @@ void SquareRender::CreateSurface() {
 
 void SquareRender::SurfaceChanged(int width, int height) {
     glViewport(0, 0, width, height);
-    float ratio = (float) width / height;
-    projectionMatrix = glm::frustum(-ratio, ratio, -1.0f, 1.0f, 3.0f, 7.0f);
-
+    float ratio = width > height ? (float) width / height : (float) height / width;
+    //以较窄的一边作为1
+    if (width > height) {
+        //透视投影。通过将场景中的物体投影到视锥体的近平面上，可以实现透视效果，使得远处的物体看起来比近处的物体小。
+        projectionMatrix = glm::frustum(-ratio, ratio, -1.0f, 1.0f, 3.0f, 7.0f);
+    } else {
+        projectionMatrix = glm::frustum(-1.0f, 1.0f, -ratio, ratio, 3.0f, 7.0f);
+    }
 }
 
 void SquareRender::DrawFrame() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
     glUseProgram(program);
-    int positionLocation = glGetAttribLocation(program, "vPosition");
     glEnableVertexAttribArray(positionLocation);
     glVertexAttribPointer(positionLocation, 3, GL_FLOAT, false, 0, trianglePoints);
 
-    int colorLocation = glGetUniformLocation(program, "vColor");
     float color_pos[] = {0.5f, 1.0f, 0.3f, 1.0f};
     glUniform4fv(colorLocation, 1, color_pos);
 
-    GLint modelMatrixLocation = glGetUniformLocation(program, "U_ModelMatrix");
     glUniformMatrix4fv(modelMatrixLocation, 1, GL_FALSE, glm::value_ptr(modelMatrix));
-    GLint viewMatrixLocation = glGetUniformLocation(program, "U_ViewMatrix");
     glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, glm::value_ptr(viewMatrix));
-    GLint projectionMatrixLocation = glGetUniformLocation(program, "U_ProjectionMatrix");
     glUniformMatrix4fv(projectionMatrixLocation, 1, GL_FALSE, glm::value_ptr(projectionMatrix));
 
     glDrawElements(GL_TRIANGLES, sizeof(triangleIndex)/sizeof(triangleIndex[0]), GL_UNSIGNED_SHORT,
--- app/src/main/cpp/SquareRender.h
+++ app/src/main/cpp/SquareRender.h
@@ -13,6 +13,11 @@ class SquareRender {
 private:
     GLuint program;
     NativeAssetsLoader *loader;
+    int positionLocation;
+    GLint modelMatrixLocation;
+    GLint viewMatrixLocation;
+    GLint projectionMatrixLocation;
+    GLint colorLocation;
     glm::mat4 modelMatrix;
     glm::mat4 viewMatrix;
     glm::mat4 projectionMatrix;
